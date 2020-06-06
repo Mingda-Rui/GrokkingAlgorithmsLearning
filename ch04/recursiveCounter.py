d776b444bfcfc27987ce29ac6d0fac2f29fae6f0
Mingda Rui <mingdarui@gmail.com>
2020-06-06 19:02:17 +0100

    answer

---
+++ ch04/recursiveCounter.py
@@ -0,0 +1,13 @@
+# EXERCISES 4.2 
+# Write a recursive function to count the number of items in a list.
+def count(arr):
+    if len(arr) <= 1: return 1
+    return 1 + count(arr[1:])
+
+def count_answer(list):
+    if list == []:
+        return 0
+    return 1 + count(list[1:])
+
+testList = [1, 2, 3, 4, 5]
+print("count the lenth of list: {}".format(count(testList)))
