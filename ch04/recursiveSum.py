f19dd82fafc7593fab486552c6cc517074a91a29
Mingda Rui <mingdarui@gmail.com>
2020-06-06 18:59:43 +0100

    answer

---
+++ ch04/recursiveSum.py
@@ -0,0 +1,18 @@
+def sum(arr):
+    total = 0
+    for x in arr:
+        total += x
+    return total
+
+def sumRecursive(arr):
+    if len(arr) <= 1: return arr[0]
+    return arr[0] + sumRecursive(arr[1:])
+
+def sum_answer(list):
+    if list == []:
+        return 0
+    return list[0] + sum(list[1:])
+
+testList = [1, 2, 3, 4, 5]
+print("sum: {}".format(sum(testList)))
+print("recursive sum: {}".format(sumRecursive(testList)))
