1249f7bb84d38188476462dc34f35d399f39bad9
Mingda Rui <mingdarui@gmail.com>
2020-06-06 01:37:49 +0100

    add comment for selection sort example

--- ch02/selectionSort.py
+++ ch02/selectionSort.py
@@ -1,5 +1,7 @@
 def findSmallest(arr):
+    # Stores the smallest value
     smallest = arr[0]
+    # Stores the index of the smallest value
     smallest_index = 0
     for i in range(1, len(arr)):
         if arr[i] < smallest:
@@ -7,9 +9,12 @@ def findSmallest(arr):
             smallest_index = i
     return smallest_index
 
+# Sorts an array
 def selectionSort(arr):
     newArr = []
     for i in range(len(arr)):
+        # Finds the smallest element in the 
+        # array, and adds it to the new array
         smallest = findSmallest(arr)
         newArr.append(arr.pop(smallest))
     return newArr
