db3350fe8ca8c14323c203d1194bc00f48585cc7
Mingda Rui <mingdarui@gmail.com>
2020-06-06 18:42:55 +0100

    refactor

--- ch03/resuriveCounter.py
+++
@@ -1,8 +0,0 @@
-# EXERCISES 4.2 
-# Write a recursive function to count the number of items in a list.
-def count(arr):
-    if len(arr) <= 1: return 1
-    return 1 + count(arr[1:])
-
-testList = [1, 2, 3, 4, 5]
-print("count the lenth of list: {}".format(count(testList)))
