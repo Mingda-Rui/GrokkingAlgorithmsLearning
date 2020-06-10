2dcef35a4e6f29c1a707714e040ff06fe0d0da98
Mingda Rui <mingdarui@gmail.com>
2020-06-10 20:48:50 +0100

    print item with sleep

--- ch04/print_item.py
+++ ch04/print_item.py
@@ -1,5 +1,10 @@
+from time import sleep
 def print_items(list):
     for item in list:
         print(item)
 
+def print_item2(list):
+    for item in list:
+        sleep(1)
+        print(item)
         
