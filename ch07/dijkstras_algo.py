ae9b6e2b22c496961a91387f9cd954fde347d722
Mingda Rui <mingdarui@gmail.com>
2020-06-21 23:14:43 +0100

    Dijkstra's Algo example

--- ch07/dijkstras_algo.py
+++ ch07/dijkstras_algo.py
@@ -1,42 +1,6 @@
 infinity = float("inf")
 
-costs = {}
-costs["a"] = 6
-costs["b"] = 2
-costs["fin"] = infinity
-
-parents = {}
-parents["a"] = "start"
-parents["b"] = "start"
-parents["fin"] = None
-
-processed = []
-
-graph = create_graph()
-
-node = find_lowest_cost_node(costs)
-while node is not None:
-    cost = costs[node]
-    neighbors = graph[node]
-    for n in neighbors.keys():
-        new_cost = cost + neighbors[n]
-        if costs[n] > new_cost:
-            costs[n] = new_cost
-            parents[n] = node
-    processed.append(node)
-    node = find_lowest_cost_node(costs)
-
-def find_lowest_cost_node(cost):
-    lowest_cost = float("inf")
-    lowest_cost_node = None
-    for node in costs:
-        cost = costs[node]
-        if cost < lowest_cost and node not in processed:
-            lowest_cost = cost
-            lowest_cost_node = node
-    return lowest_cost_node
-
-def create_graph():
+def create_graph() -> dict:
     graph = {}
     graph["start"] = {}
     graph["start"]["a"] = 6
@@ -50,4 +14,57 @@ def create_graph():
     graph["b"]["fin"] = 5
 
     graph["fin"] = {}
-    graph
+    return graph
+
+def init_costs(costs: dict) -> None:
+    costs["a"] = 6
+    costs["b"] = 2
+    costs["fin"] = infinity
+
+def init_parents(parents: dict) -> None:
+    parents["a"] = "start"
+    parents["b"] = "start"
+    parents["fin"] = None
+
+def find_lowest_cost_node(costs: dict) -> int:
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+def print_path(parents: dict) -> None:
+    current_node = "fin"
+    path = "fin"
+    while current_node in parents.keys():
+        parent_node = parents[current_node]
+        path = "{} -> {}".format(parent_node, path)
+        current_node = parent_node
+    print("shortest path: {}".format(path))
+
+# test:
+costs = {}
+parents = {}
+processed = []
+
+graph = create_graph()
+init_costs(costs)
+init_parents(parents)
+
+node = find_lowest_cost_node(costs)
+while node is not None:
+    cost = costs[node]
+    neighbors = graph[node]
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node)
+    node = find_lowest_cost_node(costs)
+
+print("Distance to fin: {}".format(costs["fin"]))
+print_path(parents)
