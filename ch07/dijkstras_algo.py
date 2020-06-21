53108f3446496f03c484a6891f85fe923ffd98be
Mingda Rui <mingdarui@gmail.com>
2020-06-21 15:46:15 +0100

    Dijkstra's Algorithm Example

--- ch07/dijkstras_algo.py
+++ ch07/dijkstras_algo.py
@@ -1,5 +1,42 @@
+infinity = float("inf")
 
-def initGraph():
+costs = {}
+costs["a"] = 6
+costs["b"] = 2
+costs["fin"] = infinity
+
+parents = {}
+parents["a"] = "start"
+parents["b"] = "start"
+parents["fin"] = None
+
+processed = []
+
+graph = create_graph()
+
+node = find_lowest_cost_node(costs)
+while node is not None:
+    cost = costs[node]
+    neighbors = graph[node]
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node)
+    node = find_lowest_cost_node(costs)
+
+def find_lowest_cost_node(cost):
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+def create_graph():
     graph = {}
     graph["start"] = {}
     graph["start"]["a"] = 6
